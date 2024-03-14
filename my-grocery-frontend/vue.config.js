5fbe55734fb71a4fc5d36ad7d988da01ff0fbe43
jalaljanjua28 <jalaljanjua88@gmail.com>
2024-03-14 12:14:31 +0500

    modified:   my-grocery-frontend/vue.config.js

--- my-grocery-frontend/vue.config.js
+++ my-grocery-frontend/vue.config.js
@@ -8,14 +8,10 @@ module.exports = {
     },
   },
   devServer: {
-    compress: true,
-    disableHostCheck: true,
     proxy: {
       "/api": {
         target: "https://my-grocery-app-hlai3cv5za-uc.a.run.app",
-        changeOrigin: true, // Add this line
         secure: false, // Add this line if your backend server uses self-signed SSL certificates
-        pathRewrite: { "^/api": "" }, // Add this line if your API doesn't have a '/api' prefix
       },
     },
   },
